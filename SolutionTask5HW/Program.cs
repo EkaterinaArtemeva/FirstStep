4457f242eb5f274f3f7d581f3ecb1ee7ca371103
EkaterinaArtemeva <109229824+EkaterinaArtemeva@users.noreply.github.com>
2022-07-29 20:53:53 +0300

    add all

---
+++ SolutionTask5HW/Program.cs
@@ -0,0 +1,18 @@
+﻿string? inputLineA = Console.ReadLine();
+string? inputLineB = Console.ReadLine();
+
+    if(inputLineA != null && inputLineB != null)
+    {
+        int inputNumberA = int.Parse(inputLineA);
+        
+        int inputNumberB = int.Parse(inputLineB);
+        
+        if(inputNumberA > inputNumberB)
+        {
+            Console.WriteLine("inputNumberA");
+        }
+        else
+        {
+            Console.WriteLine("inputNumberB");
+        }
+    }
---
+++ SolutionTask7HW/Program.cs
@@ -0,0 +1,17 @@
+﻿string? inputLine = Console.ReadLine();
+
+if(inputLine != null)
+{
+    int inputNumberA = int.Parse(inputLine);
+
+    int inputNumberB = inputNumberA%2;
+
+    if(inputNumberB == 0)
+    {
+        Console.WriteLine("yes");
+    }
+    else
+    {
+        Console.WriteLine("no");
+    }
+}
