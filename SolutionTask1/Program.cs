7e981e8391348f42863762b557ef69e5f29907a1
EkaterinaArtemeva <109229824+EkaterinaArtemeva@users.noreply.github.com>
2022-07-29 20:50:27 +0300

    add all

---
+++ SolutionTask1/Program.cs
@@ -0,0 +1,16 @@
+﻿string? inputLineOne = Console.ReadLine();
+string? inputLineTwo = Console.ReadLine();
+
+if(inputLineOne != null && inputLineTwo != null){
+    int inputNumOne = int.Parse(inputLineOne);
+    int inputNumTwo = int.Parse(inputLineTwo);
+
+    if(inputNumOne == inputNumTwo * inputNumTwo)
+    {
+        Console.WriteLine("yes");
+    }
+    else
+    {
+        Console.WriteLine("no");
+    }
+}
