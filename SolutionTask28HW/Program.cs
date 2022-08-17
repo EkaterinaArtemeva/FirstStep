f376749651aba21d2525ea69f2c5785bc6e2e340
EkaterinaArtemeva <109229824+EkaterinaArtemeva@users.noreply.github.com>
2022-08-17 17:37:45 +0300

    add all

---
+++ SolutionTask28HW/Program.cs
@@ -0,0 +1,28 @@
+﻿
+string? inputLineA = Console.ReadLine();
+string? inputLineB = Console.ReadLine();
+int NumberC;
+
+void One()
+{
+    if (inputLineA != null && inputLineB != null)
+    {
+        int inputNumberA = int.Parse(inputLineA);
+        int inputNumberB = int.Parse(inputLineB);
+        int NumberC = (int)Math.Pow(inputNumberA, inputNumberB);
+    }
+}
+
+void Two()
+{
+    int i = 0;
+    int NumberC = 1;
+    int inputNumberA = int.Parse(inputLineA);
+    int inputNumberB = int.Parse(inputLineB);
+    while (i < inputNumberB)
+        {
+            NumberC = NumberC * inputNumberA;
+        }    
+}
+
+Console.WriteLine(NumberC);
---
+++ SolutionTask29HW/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("введите число: ");
+string? inputLine = Console.ReadLine();
+int inputNumber = int.Parse(inputLine);
+
+int inputNumberB = 0;
+
+while (inputNumber > 0)
+{
+    inputNumberB = inputNumberB + inputNumber%10;
+    inputNumber = inputNumber/10;
+}
+
+Console.WriteLine(inputNumberB);
