aea0b4ba0fcd75e49f1afbad0b8d929459ba2302
EkaterinaArtemeva <109229824+EkaterinaArtemeva@users.noreply.github.com>
2022-08-17 17:31:36 +0300

    add all

---
+++ SolutionTask15HW/Program.cs
@@ -0,0 +1,17 @@
+﻿string? inputLine = Console.ReadLine();
+
+if(inputLine != null)
+    {
+        char[] numbers = inputLine.ToCharArray();
+
+         if(numbers.Length >= 2)
+         {
+            Console.WriteLine(numbers[2]);
+         }
+         else
+         {
+            Console.WriteLine("no third number");
+         }
+
+    }
+
---
+++ SolutionTask16HW/Program.cs
@@ -0,0 +1,15 @@
+﻿string? inputLine = Console.ReadLine();
+
+if(inputLine != null)
+{
+    int inputNumber = int.Parse(inputLine);
+
+    if((inputNumber == 6) || (inputNumber == 7))
+    {
+        Console.WriteLine("выходной");
+    }
+    else
+    {
+        Console.WriteLine("рабочий");
+    }
+}
