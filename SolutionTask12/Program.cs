2f74dd3a4b62e7abe28a68a37d8986585ed14c83
EkaterinaArtemeva <109229824+EkaterinaArtemeva@users.noreply.github.com>
2022-08-17 17:29:50 +0300

    add more

---
+++ SolutionTask12/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine();
+
+string? inputLineOne = Console.ReadLine();
+
+string reminderSearcher(string? str)
+{
+    if(str != null)
+    {
+        try
+        {
+            string answer;
+            int number = int.Parse(str);
+            if (((number % 7) == 0) && ((number % 23) == 0))
+            {
+                answer = "yes for 7 and 23";
+            }
+            else
+            {
+                answer = "no for 7 and 23";
+            }
+            return answer;
+            
+        }
+        catch(Exception e)
+        {
+            return "исключение: " + e.Message;
+        }
+    }
+    return "";
+}
+
+Console.WriteLine(reminderSearcher(inputLineOne));
---
+++ SolutionTask13/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine();
+
+string? inputLineA = Console.ReadLine();
+string? inputLineB = Console.ReadLine();
+
+if(inputLineA != null && inputLineB != null)
+    {
+        int inputNumberA = int.Parse(inputLineA);
+        
+        int inputNumberB = int.Parse(inputLineB);
+
+        int NumberC = inputNumberA * inputNumberA;
+         
+        int NumberD = inputNumberB * inputNumberB;
+        
+        if(NumberC == inputNumberB || NumberD == inputNumberA)
+        {
+            Console.WriteLine("yes");
+        }
+        else
+        {
+            Console.WriteLine("no");
+        }
+    }        
