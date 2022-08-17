a875a5e3f5eb70fe0584189158de3427eb13cb15
EkaterinaArtemeva <109229824+EkaterinaArtemeva@users.noreply.github.com>
2022-08-17 17:34:52 +0300

    add all

---
+++ SolutionTask21HW/Program.cs
@@ -0,0 +1,55 @@
+﻿//     int number;
+//     int digit1;
+//     int digit2;
+//     int digit3;
+//     int digit4;
+//     int digit5;
+    
+// // Считывает точки
+// void readOfPoint()
+// {
+//     int digit1 = number / 1000;
+//     int digit2 = (number / 1000) % 10;
+//     int digit3 = (number / 100) % 10;
+//     int digit4 = (number / 10) % 10;
+//     int digit5 = number % 10;
+// }
+
+// // Сравнивает пары точек
+// void compareOfPoint()
+// {
+//     if (digit1 == digit5 && digit2 == digit4)
+//     {
+//         Console.WriteLine("палиндром");
+//     }
+//     else
+//     {
+//         Console.WriteLine("не палиндром");
+//     }
+// }
+
+// readOfPoint();
+// compareOfPoint();
+
+
+string? inputLine = Console.ReadLine();
+
+if (inputLine != null)
+{
+    int number = int.Parse(inputLine);
+
+    int digit1 = number / 1000;
+    int digit2 = (number / 1000) % 10;
+    int digit3 = (number / 100) % 10;
+    int digit4 = (number / 10) % 10;
+    int digit5 = number % 10;
+
+    if (digit1 == digit5 && digit2 == digit4)
+    {
+        Console.WriteLine("палиндром");
+    }
+    else
+    {
+        Console.WriteLine("не палиндром");
+    }
+}
---
+++ SolutionTask22HW/Program.cs
@@ -0,0 +1,62 @@
+﻿// int coordXPointA;
+// int coordYPointA;
+// int coordZPointA;
+// int coordXPointB;
+// int coordYPointB;
+// int coordZPointB;
+// double lengthAB;
+
+// // Считывает координаты точек
+// void readDataOfPoint()
+// {
+//     Console.WriteLine("Введите координату X точки A");
+//     coordXPointA = int.Parse(Console.ReadLine());
+
+//     Console.WriteLine("Введите координату Y точки A");
+//     coordYPointA = int.Parse(Console.ReadLine());
+
+//     Console.WriteLine("Введите координату Z точки A");
+//     int coordZPointA = int.Parse(Console.ReadLine());
+
+//     Console.WriteLine("Введите координату X точки B");
+//     coordXPointB = int.Parse(Console.ReadLine());
+
+//     Console.WriteLine("Введите координату Y точки B");
+//     coordYPointB = int.Parse(Console.ReadLine());
+
+//     Console.WriteLine("Введите координату Z точки B");
+//     int coordZPointB = int.Parse(Console.ReadLine());
+// }
+
+// // Вычислаяет расстояние между точками
+// void calculateLengthAB()
+// {
+//     double lengthAB = Math.Sqrt(Math.Pow((coordXPointA-coordXPointB),2)+Math.Pow((coordYPointA-coordYPointB),2)+Math.Pow((coordZPointA-coordZPointB),2));
+// }
+
+// readDataOfPoint();
+// calculateLengthAB();
+
+// Console.WriteLine(lengthAB);
+
+Console.WriteLine("Введите координату X точки A");
+int coordXPointA = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите координату Y точки A");
+int coordYPointA = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите координату Z точки A");
+int coordZPointA = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите координату X точки B");
+int coordXPointB = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите координату Y точки B");
+int coordYPointB = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите координату Z точки B");
+int coordZPointB = int.Parse(Console.ReadLine());
+
+double lengthAB = Math.Sqrt(Math.Pow((coordXPointA-coordXPointB),2)+Math.Pow((coordYPointA-coordYPointB),2)+Math.Pow((coordZPointA-coordZPointB),2));
+
+Console.WriteLine(lengthAB);
